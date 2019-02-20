100f94dc4f40ef5aa96c451c09797fe73a7d273f
= <=>
2019-02-19 18:31:05 -0700

    C tone now working

--- piano.js
+++ piano.js
@@ -2,9 +2,6 @@
 console.log('hi');
 var synth = new Tone.Synth().toMaster();
 
-
-$("#C").click(function() {
-  console.log('before');
-  synth.triggerAttackRelease('C4', '8n');
-  console.log('after');
-});
+function cTone() {
+    synth.triggerAttackRelease('C4', '8n');
+}
